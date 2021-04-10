f7baf622e336ffde939262aa85e675b6a4cc745a
Satyajit Mishra <55646021+smish-hash@users.noreply.github.com>
2021-04-10 20:59:16 +0530

    Google and fb login (#6)
    * splash, onboarding, login
    
    * Google and facebook login implementation
    
    * Google and facebook login implementation
    
    Co-authored-by: PrAjJwAl KuMaR <talk2prajjwal@gmail.com>

---
+++ app/src/main/java/com/StartupBBSR/competo/Activity/MainActivity.java
@@ -0,0 +1,40 @@
+package com.StartupBBSR.competo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.StartupBBSR.competo.Activity.LoginActivity;
+import com.StartupBBSR.competo.databinding.ActivityMainBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        activityMainBinding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });
+
+
+        GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (googleSignInAccount != null){
+            activityMainBinding.textViewDemo.setText(googleSignInAccount.getDisplayName());
+        }
+    }
+}
---
+++ app/src/main/java/com/StartupBBSR/competo/Activity/SignUpActivity.java
@@ -0,0 +1,342 @@
+package com.StartupBBSR.competo.Activity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.StartupBBSR.competo.Listeners.addOnTextChangeListener;
+import com.StartupBBSR.competo.R;
+import com.StartupBBSR.competo.databinding.ActivitySignUpBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding activitySignUpBinding;
+    private int flag = 0;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseDB;
+
+    private int temp_flag = 0;
+    private String btn_clicked = "";
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activitySignUpBinding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(activitySignUpBinding.getRoot());
+
+//       Hide the action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDB = FirebaseFirestore.getInstance();
+
+        activitySignUpBinding.buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSignUp();
+            }
+        });
+
+        activitySignUpBinding.signInTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSignIn();
+            }
+        });
+
+
+        //        Google Sign in
+        activitySignUpBinding.btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_clicked = "google";
+                checkRole();
+            }
+        });
+
+//        Facebook Sign in
+        activitySignUpBinding.btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_clicked = "fb";
+                // TODO: 4/5/2021 facebookLogin();
+                Toast.makeText(getApplicationContext(), "coming soon", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+//        Text Change Listeners
+        textChangedListener(activitySignUpBinding.nameET, activitySignUpBinding.nameTIL);
+        textChangedListener(activitySignUpBinding.numberET, activitySignUpBinding.numberTIL);
+        textChangedListener(activitySignUpBinding.emailET, activitySignUpBinding.emailTIL);
+        textChangedListener(activitySignUpBinding.passwordET, activitySignUpBinding.passwordTIL);
+
+//        Check role switch
+        checkRoleSwitch();
+
+    }
+
+    private void checkRole() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+        builder.setTitle("Select Role");
+        builder.setMessage("Are you a part of any organization?");
+
+        builder.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        temp_flag = 1;
+                        Log.d("alertdialogrole", "Role: Organization");
+                        if (btn_clicked.equals("google")) {
+                            googleSignIn();
+                            googleSignInIntent();
+                        } else if (btn_clicked.equals("fb")) {
+                            // TODO: 4/5/2021 fb integration
+                        }
+
+                        dialogInterface.dismiss();
+                    }
+                }
+        );
+
+        builder.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        temp_flag = 0;
+                        Log.d("alertdialogrole", "Role: User ");
+
+                        if (btn_clicked.equals("google")) {
+                            googleSignIn();
+                            googleSignInIntent();
+                        } else if (btn_clicked.equals("fb")) {
+                            // TODO: 4/5/2021 fb integration
+                        }
+                        dialogInterface.dismiss();
+                    }
+                }
+        );
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void googleSignIn() {
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+    }
+
+    private void googleSignInIntent() {
+        Intent googleSignInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(googleSignInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//        Result from intent from googleSignIn
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+//                Google Sign In successful
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                Log.d("googlesignin: ", "firebaseAuthWithGoogle: " + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Log.d("googlesignin: ", "Google Sign In failed\n" + e.getMessage());
+                ;
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+//                            Sign in success
+                            Log.d("googlesignin", "signInWithCredential: success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            loginWithCredential(user);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "SignInWithCredential: failed", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    }
+                });
+    }
+
+    private void loginWithCredential(FirebaseUser user) {
+        String TAG = "cred";
+
+        Log.d(TAG, "loginWithCredential: Name: " + user.getDisplayName());
+        Log.d(TAG, "loginWithCredential: Email: " + user.getEmail());
+        Log.d(TAG, "loginWithCredential: Phone: " + user.getPhoneNumber());
+        if (temp_flag == 1) {
+            Log.d(TAG, "loginWithCredential: Role: Organizer");
+        } else {
+            Log.d(TAG, "loginWithCredential: Role: User");
+        }
+
+//        To add user to database if not done
+        DocumentReference documentReference = firebaseDB.collection("Users")
+                .document(firebaseAuth.getCurrentUser().getUid());
+
+        Map<String, Object> userInfo = new HashMap<>();
+        userInfo.put("Name", user.getDisplayName());
+        userInfo.put("Phone", user.getPhoneNumber());
+        userInfo.put("Email", user.getEmail());
+
+//        Now we check the role selected
+        if (temp_flag == 0)
+            userInfo.put("isUser", "1");
+        else
+            userInfo.put("isOrganizer", "1");
+
+        documentReference.set(userInfo);
+
+        if (temp_flag == 0) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        } else {
+            startActivity(new Intent(getApplicationContext(), OrganizerActivity.class));
+            finish();
+        }
+
+    }
+
+    private void onSignUp() {
+//        Check whether any field is left blank
+        flag = 0;
+        checkEmptyField(activitySignUpBinding.nameET, activitySignUpBinding.nameTIL);
+        checkEmptyField(activitySignUpBinding.numberET, activitySignUpBinding.numberTIL);
+        checkEmptyField(activitySignUpBinding.emailET, activitySignUpBinding.emailTIL);
+        checkEmptyField(activitySignUpBinding.passwordET, activitySignUpBinding.passwordTIL);
+
+
+        if (flag == 4) {
+//            We can now register the user
+            activitySignUpBinding.signUpProgressLayout.setVisibility(View.VISIBLE);
+
+            firebaseAuth.createUserWithEmailAndPassword(
+                    activitySignUpBinding.emailET.getText().toString(),
+                    activitySignUpBinding.passwordET.getText().toString()
+            ).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(SignUpActivity.this, "Account Created", Toast.LENGTH_SHORT).show();
+
+//                    Now to store data into the database
+                    DocumentReference documentReference = firebaseDB.collection("Users")
+                            .document(firebaseAuth.getCurrentUser().getUid());
+
+                    Map<String, Object> userInfo = new HashMap<>();
+                    userInfo.put("Name", activitySignUpBinding.nameET.getText().toString());
+                    userInfo.put("Phone", activitySignUpBinding.numberET.getText().toString());
+                    userInfo.put("Email", activitySignUpBinding.emailET.getText().toString());
+
+//                      Now we check the role selected from the switch
+                    if (activitySignUpBinding.roleSwitch.isChecked())
+                        userInfo.put("isUser", "1");
+                    else
+                        userInfo.put("isOrganizer", "1");
+
+
+                    documentReference.set(userInfo); // add onsuccess or onfailure here for debugging
+
+                    if (activitySignUpBinding.roleSwitch.isChecked()) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    } else {
+                        startActivity(new Intent(getApplicationContext(), OrganizerActivity.class));
+                        finish();
+                    }
+
+
+                    activitySignUpBinding.signUpProgressLayout.setVisibility(View.GONE);
+                }
+            }).addOnFailureListener(e -> {
+                Toast.makeText(SignUpActivity.this, "Account creation failed:\n" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                activitySignUpBinding.signUpProgressLayout.setVisibility(View.GONE);
+            });
+
+
+        }
+    }
+
+    private void onSignIn() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void textChangedListener(TextInputEditText ET, TextInputLayout TIL) {
+        ET.addTextChangedListener(new addOnTextChangeListener(this, ET, TIL));
+    }
+
+    private void checkEmptyField(EditText et, TextInputLayout til) {
+        if (et.getText().toString().isEmpty()) {
+            til.setError("Field cannot be blank");
+            til.setErrorEnabled(true);
+            flag--;
+        } else {
+            flag++;
+        }
+
+    }
+
+    private void checkRoleSwitch() {
+        activitySignUpBinding.roleSwitch.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b)
+                activitySignUpBinding.roleTV.setText("User");
+            else
+                activitySignUpBinding.roleTV.setText("Organizer");
+        });
+    }
+
+}
